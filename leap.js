03ec30752331dcc687c15a2e8c22e13579dfe05b
adora84 <abdulkader.kalaaji@gmail.com>
2016-04-21 16:51:27 +0200

    first try

--- leap.js
+++ leap.js
@@ -1,27 +1,5 @@
-'use strict';
-
-/**
- * Represents a year to check whether is leap or not
- *
- * @param  {number} year
- * Numeric year.
- */
-function Year(year) {
-  this.year = year;
+var YearLeap = function(year) {
+    this.year = year;
+    return (this.year % 4 === 0 && this.year % 100 === 0 || this.year % 400 === 0);
 }
-
-/**
- * Whether given year is a leap year.
- *
- * @return {boolean}
- * Whether given year is a leap year.
- */
-Year.prototype.isLeap = function () {
-  //
-  // YOUR CODE GOES HERE
-  // (you have to use this.year)
-  //
-}
-
-module.exports = Year;
-
+YearLeap(2000);
