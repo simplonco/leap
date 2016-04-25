0b8bacf3dab260ae1763a1bc9ded66801d5680af
Syed zeeshan ali zaidi <zeshanalizaidi@gmail.com>
2016-04-25 16:31:12 +0200

    Update leap.js

--- leap.js
+++ leap.js
@@ -1,14 +1,17 @@
 
-
-function Year(year) {
+var Year = function(year) {
   this.year = year;
 }
 
-Year.prototype.isLeap = function () {
-	if ( this.year % 4 == 0 && this.year % 100 != 0 && this.year % 400 != 0) {
-		console.log("boooooooo");
+Year.prototype.isLeap = function (year) {
+	if ( (year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+		return true;
 	}
+	else { return false;}
 }
-module.exports = Year;
+var test = new Year();
+console.log (test.isLeap(2000));
 
+var test2 = new Year();
+console.log (test.isLeap(4500));
 
