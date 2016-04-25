3500a2ec2ca1711e99aafc89bd4e185bcf128467
Syed zeeshan ali zaidi <zeshanalizaidi@gmail.com>
2016-04-25 12:20:33 +0200

    Update leap.js

--- leap.js
+++ leap.js
@@ -1,10 +1,4 @@
-//
-// This is only a SKELETON file for the 'Leap' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-// Make sure to look at test.script.js--that should give you some hints about what is
-// expected here.
 
-'use strict';
 
 function Year(year) {
   this.year = year;
