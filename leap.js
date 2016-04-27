9dc39fecf872c44b8595dc8ab3433e131d255a38
bmiande <mbaimiande@gmail.com>
2016-04-27 16:22:46 +0200

    Update leap.js

--- leap.js
+++ leap.js
@@ -23,10 +23,14 @@ function Year(year) {
  * Whether given year is a leap year.
  */
 Year.prototype.isLeap = function () {
-//
-// YOUR CODE GOES HERE
-// (you have to use this.year)
-//
+  var year = this.year;
+  if (year % 400 === 0) {
+          return true;
+      } else if (year % 100 === 0) {
+          return false;
+      } else if (year % 4 === 0) {
+          return true;
+      } else {
+          return false;
+      }
 }
-
-module.exports = Year;
